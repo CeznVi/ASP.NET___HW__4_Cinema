7a40c5cbdd5b89dbaea3d1bb99302fd5ed706a7b
Viktor Velychko <Cezn.Vi@gmail.com>
2023-11-04 15:13:59 +0200

    Work in progress

---
+++ Solution/ASP.CORE2023.10.28/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.CORE2023._10._28.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
